eb844cffd71899cc040f6cac6e3e78bd1be757b3
ibrahim soilahoudine <ibrahimsoilah7@gmail.com>
2024-04-10 22:30:05 +0200

    code file and readme

---
+++ cc.py
@@ -0,0 +1,13 @@
+class Truc:
+  def __init__(self, a):
+    self.a = a
+
+  def __mul__(self, autre):
+    return Truc(a=self.a * autre.a)
+
+  def __repr__(self):
+    return f"Truc({self.a})"
+
+x = Truc(1)
+y = Truc(6)
+print(x * y)
